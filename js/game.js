0653a55963086d81b3be4775a129ae71d898d6c1
nsipid <nsipid@gmail.com>
2013-04-20 22:11:25 -0500

    pre-merge commit

--- js/game.js
+++ js/game.js
@@ -10,12 +10,16 @@ var Immortal = {
         level : 0,
         step : 0,
         stepPadding : 0,
-        draw : function ( ctx ) {
+        draw : function ( ctx, collisions) {
             ctx.drawImage( Immortal.guy.img,  Immortal.guy.step * Immortal.guy.width, Immortal.guy.level * Immortal.guy.height, 
                    Immortal.guy.width, Immortal.guy.height, Immortal.guy.x, Immortal.guy.y, Immortal.guy.width, Immortal.guy.height); 
         },
-        moveLeft : function (ctx) {
-            Immortal.guy.x = Immortal.guy.x <= 0 ? 0 : Immortal.guy.x - 1;
+        moveLeft : function (ctx, collisions) {
+            var nextX = Immortal.guy.x <= 0 ? 0 : Immortal.guy.x - 1;
+            if (collisions !== undefined && Immortal.guy.checkCollision(nextX, Immortal.guy.y)) {
+                return;
+            }
+            Immortal.guy.x = nextX;
             Immortal.guy.level = 3;
             
             if ( Immortal.guy.stepPadding === 0){
@@ -23,30 +27,52 @@ var Immortal = {
             }
             
             Immortal.guy.stepPadding = (Immortal.guy.stepPadding + 1) % 10; 
-            },
-        moveRight : function (ctx){
-            Immortal.guy.x =  Immortal.guy.x +  Immortal.guy.width >= ctx.canvas.width ?  ctx.canvas.width -  Immortal.guy.width :  Immortal.guy.x + 1;
+        },
+        moveRight : function (ctx, collisions){
+            var nextX =  Immortal.guy.x +  Immortal.guy.width >= ctx.canvas.width ?  ctx.canvas.width -  Immortal.guy.width :  Immortal.guy.x + 1;
+            if (collisions !== undefined && Immortal.guy.checkCollision(nextX, Immortal.guy.y)) {
+                return;
+            }
+            Immortal.guy.x = nextX;
             Immortal.guy.level = 1;
             if ( Immortal.guy.stepPadding === 0) {
                 Immortal.guy.step = (Immortal.guy.step + 1) % 3;
             }
             Immortal.guy.stepPadding = (Immortal.guy.stepPadding + 1) % 10;
         },
-        moveDown : function (ctx){
-                Immortal.guy.y =  Immortal.guy.y +  Immortal.guy.height >= ctx.canvas.height ?   ctx.canvas.height -  Immortal.guy.height:  Immortal.guy.y + 1;
-                Immortal.guy.level = 0;
-                if ( Immortal.guy.stepPadding === 0){
-                    Immortal.guy.step = (Immortal.guy.step + 1) % 3;
-                }
+        moveDown : function (ctx, collisions){
+            var nextY =  Immortal.guy.y +  Immortal.guy.height >= ctx.canvas.height ?   ctx.canvas.height -  Immortal.guy.height:  Immortal.guy.y + 1;
+            if (collisions !== undefined && Immortal.guy.checkCollision(Immortal.guy.x, nextY)) {
+                return;
+            }
+            Immortal.guy.y = nextY;
+            Immortal.guy.level = 0;
+            if ( Immortal.guy.stepPadding === 0){
+                Immortal.guy.step = (Immortal.guy.step + 1) % 3;
+            }
             Immortal.guy.stepPadding = (Immortal.guy.stepPadding + 1) % 10;
         },
-        moveUp : function (ctx) {
-            Immortal.guy.y =  Immortal.guy.y <= 0 ?  0 : Immortal.guy.y - 1;
+        moveUp : function (ctx, collisions) {
+            var nextY =  Immortal.guy.y <= 0 ?  0 : Immortal.guy.y - 1;
+            if (collisions !== undefined && Immortal.guy.checkCollision(Immortal.guy.x, nextX)) {
+                return;
+            }
+            Immortal.guy.y = nextY;
             Immortal.guy.level = 2;
             if ( Immortal.guy.stepPadding === 0){
                 Immortal.guy.step = (Immortal.guy.step + 1) % 3;
             }
             Immortal.guy.stepPadding = (Immortal.guy.stepPadding + 1) % 10;
+        },
+        checkCollision : function (collisions, nextX, nextY){
+            for (var i = 0; i < collisions.length; i++){
+                var object = collisions[i];
+                if (object.x + object.width <= nextX || Immortal.guy.width + nextX <= object.x || object.y + object.height <= nextY || Immortal.guy.height + nextY <= object.y) {
+                    //collision
+                    return true;
+                }
+            }
+            return false;
         }
     },
 
@@ -141,7 +167,6 @@ var Immortal = {
     distance : function( obj1, obj2 ) {
         return Math.sqrt( obj1.x*obj2.x + obj1.y*obj2.y );
     }
-
 };
 
 // Keyboard 'buffer'
